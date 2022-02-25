a93374d5f5c7e0730a1958ed677a4bc5beee766a
HD <gudehdu10@gmail.com>
2022-02-25 23:56:13 +0900

    jenkins test

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn Reactaa
+          Learn React
         </a>
       </header>
     </div>
