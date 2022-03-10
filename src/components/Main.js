5e0fa091a5505d544cc68bded0e182e5c7088451
jeonchangbae <avantgard7@gmail.com>
2022-03-10 09:36:49 +0900

    main page upload

---
+++ src/components/Main.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from './Header';
+import './Main.css';
+
+function Main() {
+    return (  
+        <div className='main__container'>
+            <Header />
+        </div>
+    );
+}
+
+export default Main;
