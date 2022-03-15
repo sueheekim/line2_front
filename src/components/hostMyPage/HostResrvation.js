fe44590bd45f9a413c5c3c98fe897c054da6e5c3
tizmfk802 <tizmfk802@gmail.com>
2022-03-15 15:51:29 +0900

    LINE2-7 Add Hostreservation guest profile mapping

--- src/components/hostMyPage/HostResrvation.js
+++ src/components/hostMyPage/HostResrvation.js
@@ -1,8 +1,49 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from 'axios';
+import "./HostReservation.css";
+import { Button } from "@mui/material";
+import GuestProfile from "../guestMyPage/GuestProfile";
 
-function HostReservation() {
-    return (  
+function HostReservation( ) {
+
+    const guestUrl = "http://localhost:3006/userProfile";
+    const [userProfile, setUserProfile] = useState([]);
+
+    useEffect(()=>{
+        axios.get(guestUrl)
+        .then(res=>{
+            setUserProfile(res.data);
+            console.log(res.data);
+        });
+    },[]);
+
+
+    return (
         <>
+            <div className="host__button">
+                <Button variant="outlined" size="large">
+                    예약목록
+                </Button>
+                <Button variant="outlined" size="large">
+                    체크인
+                </Button>
+                <Button variant="outlined" size="large">
+                    체크아웃
+                </Button>
+            </div>
+
+            <div className="hostViewReservation">
+                <div className="guest_reservation_id">
+                    예약번호
+                </div>
+                <div className="guest_reservation_profile">
+                {
+                    userProfile.map((guest)=>(
+                        <GuestProfile key={guest.id} guest={guest}/>
+                        ))
+                }
+                </div>
+            </div>
         </>
     );
 }
