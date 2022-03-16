ef066c57417f0694d73f55dca0b418afce5d2c6b
tizmfk802 <tizmfk802@gmail.com>
2022-03-16 16:23:00 +0900

    LINE2-14 Edit HostCheckInCard dropdown to accordin fuction

--- src/components/hostMyPage/HostCheckInCard.js
+++ src/components/hostMyPage/HostCheckInCard.js
@@ -1,43 +1,23 @@
 import React from "react";
 import {
-    styled,
     Card,
     CardMedia,
     CardContent,
-    CardActions,
-    Collapse,
-    IconButton,
-    Typography,
-    Divider,
     Grid,
+    Accordion,
+    AccordionSummary,
+    AccordionDetails,
 } from "@mui/material";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 import "./HostCheckInCard.css";
 
-const ExpandMore = styled((props) => {
-    const { expand, ...other } = props;
-    return <IconButton {...other} />;
-})(({ theme, expand }) => ({
-    transform: !expand ? "rotate(0deg)" : "rotate(180deg)",
-    marginLeft: "auto",
-    transition: theme.transitions.create("transform", {
-        duration: theme.transitions.duration.shortest,
-    }),
-}));
-
 function HostCheckInCard() {
-    const [expanded, setExpanded] = React.useState(false);
-
-    const handleExpandClick = () => {
-        setExpanded(!expanded);
-    };
-
     return (
         <div>
             <Grid container direction="row" justifyContent="center">
                 <Card sx={{ maxWidth: 345 }}>
-                    <div className="host_checkin_card_outdate">
-                        체크 아웃 예정 일자
+                    <div className="host_checkin_card_indate">
+                        체크아웃 예정 일자
                     </div>
                     <CardMedia
                         component="img"
@@ -55,114 +35,24 @@ function HostCheckInCard() {
                         <div className="host_checkin_card_room_name">
                             체크인된 객실 이름
                         </div>
-                        <div className="host_checkin_card_indate">
-                            게스트 체크인 완료일
+                        <div className="host_checkin_card_outdate">
+                            체크인 완료일
                         </div>
                     </CardContent>
-                    <Divider />
-                    <CardActions disableSpacing>
-                        <ExpandMore
-                            expand={expanded}
-                            onClick={handleExpandClick}
-                            aria-expanded={expanded}
-                            aria-label="show more"
+                    <Accordion>
+                        <AccordionSummary
+                            expandIcon={<ExpandMoreIcon />}
+                            aria-controls="panel1a-content"
+                            id="panel1a-header"
                         >
-                            <ExpandMoreIcon />
-                        </ExpandMore>
-                    </CardActions>
-                    <Collapse in={expanded} timeout="auto" unmountOnExit>
-                        <CardContent>
-                            <Typography>
-                                게스트 체크인 특이사항들....
-                            </Typography>
-                        </CardContent>
-                    </Collapse>
-                </Card>
-                <Card sx={{ maxWidth: 345 }}>
-                    <div className="host_checkin_card_outdate">
-                        체크 아웃 예정 일자2
-                    </div>
-                    <CardMedia
-                        component="img"
-                        height="194"
-                        image=""
-                        alt="체크인된 게스트 사진2"
-                    />
-                    <CardContent>
-                        <div className="host_checkin_card_guest_name">
-                            체크인된 게스트 이름2
-                        </div>
-                        <div className="host_checkin_card_guest_gender">
-                            체크인된 게스트 성별2
-                        </div>
-                        <div className="host_checkin_card_room_name">
-                            체크인된 객실 이름2
-                        </div>
-                        <div className="host_checkin_card_indate">
-                            게스트 체크인 완료일2
-                        </div>
-                    </CardContent>
-                    <Divider />
-                    <CardActions disableSpacing>
-                        <ExpandMore
-                            expand={expanded}
-                            onClick={handleExpandClick}
-                            aria-expanded={expanded}
-                            aria-label="show more"
-                        >
-                            <ExpandMoreIcon />
-                        </ExpandMore>
-                    </CardActions>
-                    <Collapse in={expanded} timeout="auto" unmountOnExit>
-                        <CardContent>
-                            <Typography>
-                                게스트 체크인 특이사항들....
-                            </Typography>
-                        </CardContent>
-                    </Collapse>
-                </Card>
-                <Card sx={{ maxWidth: 345 }}>
-                    <div className="host_checkin_card_outdate">
-                        체크 아웃 예정 일자3
-                    </div>
-                    <CardMedia
-                        component="img"
-                        height="194"
-                        image=""
-                        alt="체크인된 게스트 사진3"
-                    />
-                    <CardContent>
-                        <div className="host_checkin_card_guest_name">
-                            체크인된 게스트 이름3
-                        </div>
-                        <div className="host_checkin_card_guest_gender">
-                            체크인된 게스트 성별3
-                        </div>
-                        <div className="host_checkin_card_room_name">
-                            체크인된 객실 이름3
-                        </div>
-                        <div className="host_checkin_card_indate">
-                            게스트 체크인 완료일3
-                        </div>
-                    </CardContent>
-                    <Divider />
-                    <CardActions disableSpacing>
-                        <ExpandMore
-                            expand={expanded}
-                            onClick={handleExpandClick}
-                            aria-expanded={expanded}
-                            aria-label="show more"
-                        >
-                            <ExpandMoreIcon />
-                        </ExpandMore>
-                    </CardActions>
-                    <Collapse in={expanded} timeout="auto" unmountOnExit>
-                        <CardContent>
-                            <Typography>
-                                게스트 체크인 특이사항들....
-                            </Typography>
-                        </CardContent>
-                    </Collapse>
+                            <div className="host_checkin_card_outdate">
+                                체크인 특이사항
+                            </div>
+                        </AccordionSummary>
+                        <AccordionDetails>
+                            <p>경찰서에서 인수인계됨</p>
+                        </AccordionDetails>
+                    </Accordion>
                 </Card>
             </Grid>
         </div>
