e458e32ae26a7570decd38d95e38eea0304fce54
tizmfk802 <tizmfk802@gmail.com>
2022-03-16 16:29:35 +0900

    LINE2-14 Edit HostCheckInCard dropdown to accordin fuction

--- src/components/hostMyPage/HostCheckInCard.js
+++ src/components/hostMyPage/HostCheckInCard.js
@@ -54,7 +54,86 @@ function HostCheckInCard() {
                         </AccordionDetails>
                     </Accordion>
                 </Card>
+                <Card sx={{ maxWidth: 345 }}>
+                    <div className="host_checkin_card_outdate">
+                        체크아웃 예정 일자2
+                    </div>
+                    <CardMedia
+                        component="img"
+                        height="194"
+                        image=""
+                        alt="체크인된 게스트 사진2"
+                    />
+                    <CardContent>
+                        <div className="host_checkin_card_guest_name">
+                            체크인된 게스트 이름2
+                        </div>
+                        <div className="host_checkin_card_guest_gender">
+                            체크인된 게스트 성별2
+                        </div>
+                        <div className="host_checkin_card_room_name">
+                            체크인된 객실 이름2
+                        </div>
+                        <div className="host_checkin_card_indate">
+                            체크인 완료일2
+                        </div>
+                    </CardContent>
+                    <Accordion>
+                        <AccordionSummary
+                            expandIcon={<ExpandMoreIcon />}
+                            aria-controls="panel1a-content"
+                            id="panel1a-header"
+                        >
+                            <div className="host_checkin_card_indate">
+                                체크인 특이사항2
+                            </div>
+                        </AccordionSummary>
+                        <AccordionDetails>
+                            <p>타 지역에서 옴</p>
+                        </AccordionDetails>
+                    </Accordion>
+                </Card>
+                <Card sx={{ maxWidth: 345 }}>
+                    <div className="host_checkin_card_outdate">
+                        체크아웃 예정 일자3
+                    </div>
+                    <CardMedia
+                        component="img"
+                        height="194"
+                        image=""
+                        alt="체크인된 게스트 사진3"
+                    />
+                    <CardContent>
+                        <div className="host_checkin_card_guest_name">
+                            체크인된 게스트 이름3
+                        </div>
+                        <div className="host_checkin_card_guest_gender">
+                            체크인된 게스트 성별3
+                        </div>
+                        <div className="host_checkin_card_room_name">
+                            체크인된 객실 이름3
+                        </div>
+                        <div className="host_checkin_card_indate">
+                            체크인 완료일3
+                        </div>
+                    </CardContent>
+                    <Accordion>
+                        <AccordionSummary
+                            expandIcon={<ExpandMoreIcon />}
+                            aria-controls="panel1a-content"
+                            id="panel1a-header"
+                        >
+                            <div className="host_checkin_card_indate">
+                                체크인 특이사항3
+                            </div>
+                        </AccordionSummary>
+                        <AccordionDetails>
+                            <p>가정을 떠난지 한달이 지난 상태에서 옴</p>
+                        </AccordionDetails>
+                    </Accordion>
+                </Card>
             </Grid>
+            
         </div>
     );
 }
