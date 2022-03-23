6e1e652aa19c096184e0eae46e91c26f8b5534ff
HD <gudehdu10@gmail.com>
2022-03-23 10:48:53 +0900

    test

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,10 +4,14 @@ module.exports = function (app) {
     app.use(
         '/book',
         createProxyMiddleware({
-            // target: 'http://was:8080',
-            // target: 'http://localhost:8080',
-            // target: 'http://tomcat:8080',
             target: 'http://openjdk:8080',
+            changeOrigin: true
+        })
+    )
+    app.use(
+        '/book',
+        createProxyMiddleware({
+            target: 'http://openjdk2:8080',
             changeOrigin: true
         })
     )
