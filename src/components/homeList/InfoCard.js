16485ce32411c69e1b6fd171ad4c6ed1cbe3b283
HD <gudehdu10@gmail.com>
2022-03-26 16:09:23 +0900

    add info card facility icon

--- src/components/homeList/InfoCard.js
+++ src/components/homeList/InfoCard.js
@@ -1,32 +1,39 @@
 import React from 'react'
 import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
 import './InfoCard.css';
-import { useNavigate } from 'react-router-dom';
+import {useNavigate} from 'react-router-dom';
 
-function InfoCard({shelter , homeFacilities}) {
+function InfoCard({shelter, homeFacilities}) {
     const navigate = useNavigate();
 
     return (
-    <div className='list_container'>
-        <div className=''>
-            <img src={`img/${shelter.images[0]}`} layout='fill' objectFit='cover' className='rounded-2xl' alt='roomImg'/>
-        </div>
-
-        <div className='card_info'>
-            <div className='card_info-category'>
-                <h4>{shelter.homeName}</h4>
-                <p>{shelter.homeCategoryName}</p>
-                <FavoriteBorderIcon />
+        <div className='list_container'>
+            <div className=''>
+                <img src={`img/${shelter.image}`} layout='fill' objectFit='cover' className='rounded-2xl'
+                     alt='roomImg'/>
             </div>
-            <p>{shelter.homeAddress}</p>
-            <p >{shelter.maxHeadCount}</p>
-                <div className='card_bottom'>
-                    <p>{homeFacilities}</p>
-                    <button onClick={()=>navigate(`/home_reservation/${shelter.id}`)}>예약 가능 옵션 보기</button>
+
+            <div className='card_info'>
+                <div className='card_info-category'>
+                    <h4>{shelter.homeName}</h4>
+                    <p>{shelter.homeCategoryName}</p>
+                    <FavoriteBorderIcon/>
                 </div>
+                <p>{shelter.homeAddress}</p>
+                <p>{shelter.maxHeadCount}</p>
+                <div className='card_bottom'>
+                    <p>
+                        {
+                            homeFacilities.map(homeFacility => (
+                                <img className={"facility_icon"} src={`/img/facility${homeFacility}.svg`} alt={homeFacility}/>
+                            ))
+                        }
+                    </p>
+                    <button onClick={() => navigate(`/home_reservation/${shelter.id}`)}>예약 가능 옵션 보기</button>
+                </div>
+            </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default InfoCard
