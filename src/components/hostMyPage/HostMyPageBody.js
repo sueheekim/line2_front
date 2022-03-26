adca56c23e19c19932d8c99193bea1719cb4cd85
tizmfk802 <tizmfk802@gmail.com>
2022-03-26 11:27:31 +0900

    Apply SPM style with .prettierrc.js

--- src/components/hostMyPage/HostMyPageBody.js
+++ src/components/hostMyPage/HostMyPageBody.js
@@ -1,5 +1,5 @@
-import {Tab, Tabs } from '@mui/material';
-import React, { useState } from "react";
+import { Tab, Tabs } from '@mui/material';
+import React, { useState } from 'react';
 import './HostMyPageBody.css';
 import HostChatting from './HostChatting';
 import HostHome from './HostHome';
@@ -11,29 +11,23 @@ function HostMyPageBody() {
 
     const handleChange = (event, newValue) => {
         setValue(newValue);
-      };
+    };
 
     return (
-        <div className='hostmypage__body'>
-            <div className='hostmypage__tabs'>
-                
-                    <Tabs
-                        className='tabs'
-                        value={value}
-                        onChange={handleChange}
-                    >
-                        <Tab style={{fontSize : '30px'}} label="예약" />
-                        <Tab style={{fontSize : '30px'}} label="숙소" />
-                        <Tab style={{fontSize : '30px'}} label="채팅" />
-                        <Tab style={{fontSize : '30px'}} label="계정" />
-                    </Tabs>
+        <div className="hostmypage__body">
+            <div className="hostmypage__tabs">
+                <Tabs className="tabs" value={value} onChange={handleChange}>
+                    <Tab style={{ fontSize: '30px' }} label="예약" />
+                    <Tab style={{ fontSize: '30px' }} label="숙소" />
+                    <Tab style={{ fontSize: '30px' }} label="채팅" />
+                    <Tab style={{ fontSize: '30px' }} label="계정" />
+                </Tabs>
             </div>
-                    {value === 0 && <HostReservation /> }
-                    {value === 1 && <HostHome /> }
-                    {value === 2 && <HostChatting /> }
-                    {value === 2 && <HostProfile /> }
-            <div className='myReservation__content'>
-            </div>
+            {value === 0 && <HostReservation />}
+            {value === 1 && <HostHome />}
+            {value === 2 && <HostChatting />}
+            {value === 2 && <HostProfile />}
+            <div className="myReservation__content"></div>
         </div>
     );
 }
