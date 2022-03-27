974de8b55b7f16e5171bf00da1e5121baf406a5d
tizmfk802 <tizmfk802@gmail.com>
2022-03-27 15:40:44 +0900

    LINE2-7 Edit GuestMyPageBody CSS

--- src/components/guestMyPage/GuestMyPageMain.js
+++ src/components/guestMyPage/GuestMyPageMain.js
@@ -1,12 +1,9 @@
 import React from 'react';
 import GuestMyPageBody from './GuestMyPageBody';
-import GuestMyPageHeader from './GuestMyPageHeader';
-import './GuestMyPageMain.css';
 
 function GuestMyPageMain() {
     return (  
         <div className='guestmypage'>
-            {/* <GuestMyPageHeader /> */}
             <GuestMyPageBody />
         </div>
     );
--- src/components/hostMyPage/HostMyPageBody.js
+++ src/components/hostMyPage/HostMyPageBody.js
@@ -1,5 +1,5 @@
-import {Tab, Tabs} from '@mui/material';
-import React, {useState} from "react";
+import { Tab, Tabs } from '@mui/material';
+import React, { useState } from 'react';
 import './HostMyPageBody.css';
 import HostChatting from './HostChatting';
 import HostHome from './HostHome';
@@ -14,33 +14,33 @@ function HostMyPageBody() {
     };
 
     return (
-        <div className='hostmypage__body'>
-            <div className='hostmypage__tabs'>
-
-                <Tabs
-                    className='tabs'
-                    value={value}
-                    onChange={handleChange}
-                >
-                    <Tab style={{fontSize: '30px', color: '#ffffff'}} label="예약"/>
-                    <Tab style={{fontSize: '30px', color: '#ffffff'}} label="숙소"/>
-                    <Tab style={{fontSize: '30px', color: '#ffffff'}} label="채팅"/>
-                    <Tab style={{fontSize: '30px', color: '#ffffff'}} label="계정"/>
+        <div className="hostmypage__body">
+            <div className="hostmypage__tabs">
+                <Tabs className="tabs" value={value} onChange={handleChange}>
+                    <Tab
+                        style={{ fontSize: '30px', color: '#ffffff' }}
+                        label="예약"
+                    />
+                    <Tab
+                        style={{ fontSize: '30px', color: '#ffffff' }}
+                        label="숙소"
+                    />
+                    <Tab
+                        style={{ fontSize: '30px', color: '#ffffff' }}
+                        label="채팅"
+                    />
+                    <Tab
+                        style={{ fontSize: '30px', color: '#ffffff' }}
+                        label="계정"
+                    />
                 </Tabs>
             </div>
-            <div className={"container"}>
-                {value === 0 && <HostReservation/>}
-                {value === 1 && <HostHome/>}
-                {value === 2 && <HostChatting/>}
-                {value === 2 && <HostProfile/>}
+            <div className={'container'}>
+                {value === 0 && <HostReservation />}
+                {value === 1 && <HostHome />}
+                {value === 2 && <HostChatting />}
+                {value === 2 && <HostProfile />}
             </div>
-            <div className='myReservation__content'>
-            </div>
-            {value === 0 && <HostReservation />}
-            {value === 1 && <HostHome />}
-            {value === 2 && <HostChatting />}
-            {value === 2 && <HostProfile />}
-            <div className="myReservation__content"></div>
         </div>
     );
 }
