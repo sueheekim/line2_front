3d86a1972b96e35b0056dc0eb471bd30592ce154
tizmfk802 <tizmfk802@gmail.com>
2022-03-27 15:44:32 +0900

    LINE2-7 Delete GuestMyPageHeader

--- src/components/guestMyPage/GuestMyPageMain.js
+++ src/components/guestMyPage/GuestMyPageMain.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import GuestMyPageBody from './GuestMyPageBody';
-import GuestMyPageHeader from './GuestMyPageHeader';
 import './GuestMyPageMain.css';
 
 function GuestMyPageMain() {
     return (  
         <div className='guestmypage'>
-            {/* <GuestMyPageHeader /> */}
             <GuestMyPageBody />
         </div>
     );
