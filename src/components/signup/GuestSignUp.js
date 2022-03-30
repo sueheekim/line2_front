edf911efc004c4b758a9c5be91ad68058f638e46
tizmfk802 <tizmfk802@gmail.com>
2022-03-30 11:22:15 +0900

    LINE2-8 Add guset sign up registration completed button fuction

--- src/components/signup/GuestSignUp.js
+++ src/components/signup/GuestSignUp.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
     Radio,
     RadioGroup,
@@ -10,16 +10,24 @@ import {
 import './GuestSignUp.css';
 
 function GuestSignUp() {
-    const [value, setValue] = React.useState('');
+    const [user, setUser] = useState({
+        loginName: '',
+        password: '',
+        userName: '',
+        userGender: '',
+        userPhoneNumber: '',
+        userEmail: '',
+        userImg: '',
+    });
 
     const handleChange = event => {
-        setValue(event.target.value);
+        setUser(event.target.user);
     };
 
     return (
         <>
-            <h1>게스트 회원가입</h1>
             <div className={'container'}>
+                <h1>안심 서비스의 게스트 되기</h1>
                 <div>
                     <h2>사용하실 아이디를 입력하세요</h2>
                     <TextField
@@ -28,7 +36,7 @@ function GuestSignUp() {
                         id="outlined-multiline-flexible"
                         placeholder="영문+숫자 조합 최소 8자리"
                         maxRows={4}
-                        value={value}
+                        user={user}
                         onChange={handleChange}
                     />
                     <h2>사용하실 비밀번호를 입력하세요</h2>
@@ -39,7 +47,7 @@ function GuestSignUp() {
                         placeholder="영문+숫자 조합 최소 8자리"
                         type="password"
                         autoComplete="current-password"
-                        value={value}
+                        user={user}
                         onChange={handleChange}
                     />
                     <h2>성함을 입력하세요(신분증과 일치해야 합니다.)</h2>
@@ -50,21 +58,23 @@ function GuestSignUp() {
                         placeholder="예)김미영"
                         multiline
                         maxRows={4}
-                        value={value}
+                        user={user}
                         onChange={handleChange}
                     />
                     <h2>성별을 선택하세요</h2>
                     <FormControl>
                         <RadioGroup row>
                             <FormControlLabel
-                                value="female"
+                                user={user}
                                 control={<Radio />}
                                 label="여자"
+                                onChange={handleChange}
                             />
                             <FormControlLabel
-                                value="male"
+                                user={user}
                                 control={<Radio />}
                                 label="남자"
+                                onChange={handleChange}
                             />
                         </RadioGroup>
                     </FormControl>
@@ -76,7 +86,7 @@ function GuestSignUp() {
                         placeholder="예)000-0000-0000"
                         multiline
                         maxRows={4}
-                        value={value}
+                        user={user}
                         onChange={handleChange}
                     />
                     <h2>이메일 주소를 입력하세요</h2>
@@ -87,17 +97,24 @@ function GuestSignUp() {
                         placeholder="예)abced@gmail.com"
                         multiline
                         maxRows={4}
-                        value={value}
+                        user={user}
                         onChange={handleChange}
                     />
 
                     <h2>본인 인증 사진을 업로드 해주세요</h2>
                     <h3>쉼터 체크인 시 본인 확인의 용도로만 사용 됩니다.</h3>
                     <div className="guest_identity_photo">
-                    <div className="guest_identity_h4">본인임을 증명 할 수 있는 신분증 사진을 업로드 해주세요<br/>예) 학생증, 청소년증, 민증, 운전면허증 등등
-                    </div>
+                        <div className="guest_identity_h4">
+                            본인임을 증명 할 수 있는 신분증 사진을 업로드
+                            해주세요
+                            <br />
+                            예) 학생증, 청소년증, 민증, 운전면허증 등등
+                        </div>
                         <input type="file" id="userImg" />
                         <Button variant="contained">사진 업로드</Button>
+                    </div>
+                    <div className="guest_identity_button">
+                    <Button variant="outlined">게스트 등록하기</Button>
                     </div>
                 </div>
             </div>
