3651fe93dca7a58fb889aeb45e68475b32abd343
tizmfk802 <tizmfk802@gmail.com>
2022-03-31 14:12:36 +0900

    LINE2-16 Add guest checkout card button fuction

--- src/components/guestMyPage/GuestCheckOutCard.js
+++ src/components/guestMyPage/GuestCheckOutCard.js
@@ -1,28 +1,40 @@
 import React from 'react';
-import { Card, CardContent, Typography, Button } from '@mui/material';
+import { Card, CardContent, Button, Grid } from '@mui/material';
 import './GuestCheckOutCard.css';
 
 function GuestCheckOutCard({ reservation }) {
     return (
         <>
             <h3>체크아웃</h3>
-            <Card sx={{ maxWidth: 1000 }}>
+            <Card sx={{ maxWidth: 1130 }}>
                 <CardContent>
-                <div className="guest_checkoutcard_title">
+                    <div className="guest_checkoutcard_title">
                         {reservation.userName}님의 체크아웃 일정입니다.
-                        </div>
+                    </div>
                     <div className="guest_checkoutcard_checkin">
-                            체크인 확정 날짜 : {reservation.checkInDate}
-                        </div>
-                        <div className="guest_checkoutcard_menu">{reservation.shelter_name}</div>
-                        <div className="guest_checkoutcard_menu">{reservation.roomName}</div>
-                        <div className="guest_checkoutcard_checkout">
-                            체크아웃 예정 날짜 : {reservation.checkOutDate}
-                        </div>
+                        체크인 확정 날짜 : {reservation.checkInDate}
+                    </div>
+                    <div className="guest_checkoutcard_menu">
+                        {reservation.shelter_name}
+                    </div>
+                    <div className="guest_checkoutcard_menu">
+                        {reservation.roomName}
+                    </div>
+                    <div className="guest_checkoutcard_checkout">
+                        체크아웃 예정 날짜 : {reservation.checkOutDate}
+                    </div>
                 </CardContent>
-                <Button variant="contained" color="error">
-                    체크 아웃 하기
-                </Button>
+                <Grid
+                    container
+                    direction="row"
+                    justifyContent="flex-end"
+                    alignItems="flex-end"
+                >
+                    <div className="guest_checkoutcard_h4">오늘 퇴실 하실 예정이라면 체크아웃 하기를 클릭하세요</div>
+                    <Button variant="contained" color="error">
+                        체크 아웃 하기
+                    </Button>
+                </Grid>
             </Card>
         </>
     );
