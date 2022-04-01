769bfd67e1b6af3ebf9c5a37dd8cea135a7a8986
tizmfk802 <tizmfk802@gmail.com>
2022-04-01 15:41:07 +0900

    LINE2-21 Delete GuestReservation.js guest profile map fuction

--- src/components/guestMyPage/GuestReservation.js
+++ src/components/guestMyPage/GuestReservation.js
@@ -7,21 +7,11 @@ import GuestRecentReservation from './GuestRecentReservation';
 import GuestPreviousRservation from './GuestPreviousReservation';
 
 function GuestReservation() {
-    const guestUrl = "/book/v1/user/1";
     const guestReservationUrl = "/book/v1/reservation/user/1";
     const guestPreviousReservationUrl = "/book/v1/reservation/user/1";
 
-    const [userProfile, setUserProfile] = useState([]);
     const [userReservation, setUserReservation] = useState([]);
     const [userPreviousReservation, setUserPreviousReservation] = useState([]);
-
-    useEffect(()=>{
-        axios.get(guestUrl)
-        .then(res=>{
-            setUserProfile(res.data);
-            console.log(res.data);
-        });
-    },[]);
 
     useEffect(()=>{
         axios.get(guestReservationUrl)
@@ -42,13 +32,7 @@ function GuestReservation() {
     return (
         <>
         <div className='guest__info'>
-            <div className='guestProfile'>
-                {
-                    userProfile.map(guest=>(
-                        <GuestProfile key={guest.id} guest={guest}/>
-                        ))
-                }
-            </div>
+            
             <div className='guestReservation'>
                 {
                     userReservation.map((reservation)=>(
