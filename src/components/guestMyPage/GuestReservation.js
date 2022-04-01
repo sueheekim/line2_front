8b94b5b46ac77967ef6d2d1d29aa885795bfba15
tizmfk802 <tizmfk802@gmail.com>
2022-04-01 15:48:10 +0900

    LINE2-21 Edit GuestRservation.CSS

--- src/components/guestMyPage/GuestReservation.js
+++ src/components/guestMyPage/GuestReservation.js
@@ -1,55 +1,52 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
-import GuestProfile from './GuestProfile';
 import './GuestReservation.css';
 import GuestAlarmHome from './GuestAlarmHome';
 import GuestRecentReservation from './GuestRecentReservation';
 import GuestPreviousRservation from './GuestPreviousReservation';
 
 function GuestReservation() {
-    const guestReservationUrl = "/book/v1/reservation/user/1";
-    const guestPreviousReservationUrl = "/book/v1/reservation/user/1";
+    const guestReservationUrl = '/book/v1/reservation/user/1';
+    const guestPreviousReservationUrl = '/book/v1/reservation/user/1';
 
     const [userReservation, setUserReservation] = useState([]);
     const [userPreviousReservation, setUserPreviousReservation] = useState([]);
 
-    useEffect(()=>{
-        axios.get(guestReservationUrl)
-        .then(res=>{
+    useEffect(() => {
+        axios.get(guestReservationUrl).then(res => {
             setUserReservation(res.data);
-            console.log(res.data)
+            console.log(res.data);
         });
-    },[]);
+    }, []);
 
-    useEffect(()=>{
-        axios.get(guestPreviousReservationUrl)
-        .then(res=>{
+    useEffect(() => {
+        axios.get(guestPreviousReservationUrl).then(res => {
             setUserPreviousReservation(res.data);
-            console.log(res.data)
+            console.log(res.data);
         });
-    },[]);
+    }, []);
 
     return (
         <>
-        <div className='guest__info'>
-            
-            <div className='guestReservation'>
-                {
-                    userReservation.map((reservation)=>(
-                        <GuestRecentReservation key={reservation.id} reservation={reservation}/>
-                    ))
-                }
+            <div className="guestReservation">
+                {userReservation.map(reservation => (
+                    <GuestRecentReservation
+                        key={reservation.id}
+                        reservation={reservation}
+                    />
+                ))}
             </div>
-        </div>
-        <div className='guestAlarm__home'>
-            <GuestAlarmHome />
-        </div>
-        <div className='guestReservation'>
-                {
-                    userPreviousReservation.map((reservation)=>(
-                        <GuestPreviousRservation key={reservation.id} reservation={reservation}/>
-                    ))
-                }
+
+            <div className="guestAlarm__home">
+                <GuestAlarmHome />
+            </div>
+            <div className="guestReservation">
+                {userPreviousReservation.map(reservation => (
+                    <GuestPreviousRservation
+                        key={reservation.id}
+                        reservation={reservation}
+                    />
+                ))}
             </div>
         </>
     );
