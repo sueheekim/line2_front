f3147aae53447f7442f957d433196d6fa515f0c8
tizmfk802 <tizmfk802@gmail.com>
2022-04-01 16:56:33 +0900

    LINE2-7 Edit GuestRecentRservation.js map fuction delete and api url edit

--- src/components/guestMyPage/GuestRecentReservation.js
+++ src/components/guestMyPage/GuestRecentReservation.js
@@ -1,34 +1,35 @@
 import { Button } from "@mui/material";
 import React from "react";
-import "./GuestRecentReservation.css";
+import "./GuestReservation.css";
 
-function GuestRecentReservation({ reservation }) {
+function GuestRecentReservation({ GuestReservation }) {
+
     return (
         <div className="guestRecentReservation">
             <h3>최근 예약</h3>
             <div className="reservation__container">
                 <div className="room_img">
                     <div className="shelter_name">
-                        {reservation.shelter_name}
+                        {GuestReservation.shelter_name}
                     </div>
-                    <img src={`img/${reservation.homeImg}`} alt="user.png" />
+                    <img src={`img/${GuestReservation.homeImg}`} alt="user.png" />
                 </div>
                 <div className="shelter_location">
-                    {reservation.homeName}
+                    {GuestReservation.homeName}
                 </div>
-                <div className="room_name">{reservation.roomName}</div>
-                <div className="checkIn_date">{reservation.checkInDate}</div>
-                <div className="checkOut_date">{reservation.checkOutDate}</div>
+                <div className="room_name">{GuestReservation.roomName}</div>
+                <div className="checkIn_date">{GuestReservation.checkInDate}</div>
+                <div className="checkOut_date">{GuestReservation.checkOutDate}</div>
             </div>
             <div className="reservation_info">
                 <div className="reservation_id">
-                    예약번호 : {reservation.id}
+                    예약번호 : {GuestReservation.id}
                 </div>
                 <div className="reservation_date">
-                    예약 상세 정보 : {reservation.id}
+                    예약 상세 정보 : {GuestReservation.id}
                 </div>
                 <div className="reservation_username">
-                    예약자 : {reservation.userName}
+                    예약자 : {GuestReservation.userName}
                 </div>
             </div>
             <div className="reservation__button">
--- src/components/guestMyPage/GuestReservation.js
+++ src/components/guestMyPage/GuestReservation.js
@@ -1,53 +1,29 @@
-import axios from 'axios';
 import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import './GuestReservation.css';
 import GuestAlarmHome from './GuestAlarmHome';
 import GuestRecentReservation from './GuestRecentReservation';
 import GuestPreviousRservation from './GuestPreviousReservation';
 
 function GuestReservation() {
-    const guestReservationUrl = '/book/v1/reservation/user/1';
-    const guestPreviousReservationUrl = '/book/v1/reservation/user/1';
 
-    const [userReservation, setUserReservation] = useState([]);
-    const [userPreviousReservation, setUserPreviousReservation] = useState([]);
+    const guestRecentReservationUrl = '/book/v1/reservation/user/1';
+
+    const [guestRecentReservation, setGuestRecentReservation] = useState([]);
 
     useEffect(() => {
-        axios.get(guestReservationUrl).then(res => {
-            setUserReservation(res.data);
+        axios.get(guestRecentReservationUrl).then(res => {
+            setGuestRecentReservation(res.data);
             console.log(res.data);
         });
     }, []);
-
-    useEffect(() => {
-        axios.get(guestPreviousReservationUrl).then(res => {
-            setUserPreviousReservation(res.data);
-            console.log(res.data);
-        });
-    }, []);
-
     return (
         <>
             <div className="guestReservation">
-                {userReservation.map(reservation => (
-                    <GuestRecentReservation
-                        key={reservation.id}
-                        reservation={reservation}
-                    />
-                ))}
+                    <GuestRecentReservation GuestReservation ={GuestReservation} />
             </div>
 
-            <div className="guestAlarm__home">
-                <GuestAlarmHome />
-            </div>
-            <div className="guestReservation">
-                {userPreviousReservation.map(reservation => (
-                    <GuestPreviousRservation
-                        key={reservation.id}
-                        reservation={reservation}
-                    />
-                ))}
-            </div>
+           
         </>
     );
 }
