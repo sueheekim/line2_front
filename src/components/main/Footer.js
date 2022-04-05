10db2be16e02ecea4dc49ecd3243f485f6478443
tizmfk802 <tizmfk802@gmail.com>
2022-04-05 14:20:26 +0900

    LINE2-286 Edit footer css standardization

--- src/components/main/Footer.js
+++ src/components/main/Footer.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import './Footer.css';
 import logo from '../../images/logo.jpg'
 import facebook from '../../images/facebook.png';
 import instagram from '../../images/instagram.png';
@@ -9,16 +8,16 @@ import github from '../../images/github.png';
 function Footer() {
     return (  
         <div className='footer'>
-            <div className='footer__container'>
-                <div className='footer__logo'>
+            <div className='footer_container'>
+                <div className='footer_logo'>
                     <img src={logo} alt='logo.jpg'/>
                 </div>
-                <div className='footer__contact'>
+                <div className='footer_contact'>
                     <h1>Contact Us</h1>
                     <h4>Hp :</h4>
                     <h4>E-mail :</h4>
                 </div>
-                <div className='footer__sns'>
+                <div className='footer_sns'>
                     <img src={facebook} alt="facebook.png"/>
                     <img src={instagram} alt="instagram.png"/>
                     <img src={twitter} alt="twitter.png"/>
