85063d8d857428df72fa2fd66fa88fd1d7d0619b
tizmfk802 <tizmfk802@gmail.com>
2022-04-05 14:35:34 +0900

    LINE2-286 Edit main css standardization

--- src/components/main/Banner.js
+++ src/components/main/Banner.js
@@ -1,14 +1,13 @@
 import React from 'react';
-import './Banner.css';
 import bgImg from '../../images/background3.jpg'
 
 function Banner() {
   return (
     <div  >
-        <div className='banner__img'>
+        <div className='banner_img'>
             {/* <img src='http://links.papareact.com/0fm' alt='banner'/> */}
               <img src={bgImg} alt='banner'/>
-            {/* <div className='banner__text'>
+            {/* <div className='banner_text'>
             <p>Not sure where to go? Ansim.</p>
             <button>Come here</button>
             </div> */}
