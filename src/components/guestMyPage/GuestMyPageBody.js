8689c15762e63b775cf8db6eda447637f5ab6e99
tizmfk802 <tizmfk802@gmail.com>
2022-04-12 17:38:40 +0900

    LINE2-9 Add guest mypage tab style props fuction

--- src/components/guestMyPage/GuestMyPageBody.js
+++ src/components/guestMyPage/GuestMyPageBody.js
@@ -1,10 +1,34 @@
 import React, { useState } from 'react';
+import { styled } from '@mui/material/styles';
 import { Tab, Tabs } from '@mui/material';
 import { Face, Hotel, Star } from '@mui/icons-material';
 import GuestReview from './GuestReview';
 import GuestAccount from './GuestAccount';
 import GuestReservation from './GuestReservation';
 
+const StyledTabs = styled((props) => (
+    <Tabs
+      {...props}
+      TabIndicatorProps={{ children: <span className="MuiTabs-indicatorSpan" /> }}
+    />
+  ))({
+    '& .MuiTabs-indicator': {
+      display: 'flex',
+      justifyContent: 'center',
+      backgroundColor: 'transparent',
+    },
+    '& .MuiTabs-indicatorSpan': {
+      maxWidth: 60,
+      width: '100%',
+      backgroundColor: 'white',
+    },
+  });
+  
+  const StyledTab = styled((props) => <Tab disableRipple {...props} />)(
+    ({ theme }) => ({
+      marginRight: theme.spacing(8)
+    }),
+  );
 function GuestMyPageBody() {
     const [value, setValue] = useState(0);
 
@@ -15,7 +39,7 @@ function GuestMyPageBody() {
     return (
         <div className="mypage_body">
             <div className="mypage_tabs">
-                <Tabs
+                <StyledTabs
                     value={value}
                     TabIndicatorProps={{
                         sx: {
@@ -25,22 +49,22 @@ function GuestMyPageBody() {
                     onChange={handleChange}
                     aria-label="icon label tabs example"
                 >
-                    <Tab
+                    <StyledTab
                         icon={<Hotel style={{ fontSize: '50px' }} />}
                         style={{ fontSize: '25px', color: '#ffffff' }}
                         label="예약"
                     />
-                    <Tab
+                    <StyledTab
                         icon={<Star style={{ fontSize: '50px' }} />}
                         style={{ fontSize: '25px', color: '#ffffff' }}
                         label="후기"
                     />
-                    <Tab
+                    <StyledTab
                         icon={<Face style={{ fontSize: '50px' }} />}
                         style={{ fontSize: '25px', color: '#ffffff' }}
                         label="계정"
                     />
-                </Tabs>
+                </StyledTabs>
             </div>
             <div className={'container'}>
                 {value === 0 && <GuestReservation />}
