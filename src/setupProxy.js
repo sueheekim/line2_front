500d82f878b78a7bbc87081d1e59051fcfb9ff2b
jeonchangbae <avantgard7@gmail.com>
2022-04-14 10:01:55 +0900

    LINE2 userLogin reservation edit

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,32 +4,32 @@ module.exports = function (app) {
     app.use(
         '/book',
         createProxyMiddleware({
-            // target: 'http://openjdk:8080',
-            target: 'http://localhost:8080',
+            target: 'http://openjdk:8080',
+            // target: 'http://localhost:8080',
             changeOrigin: true
         })
     )
     app.use(
         '/user',
         createProxyMiddleware({
-            // target: 'http://openjdk2:8080',
-            target: 'http://localhost:8081',
+            target: 'http://openjdk2:8080',
+            // target: 'http://localhost:8081',
             changeOrigin: true
         })
     )
     app.use(
         '/home',
         createProxyMiddleware({
-            // target: 'http://openjdk3:8080',
-            target: 'http://localhost:8082',
+            target: 'http://openjdk3:8080',
+            // target: 'http://localhost:8082',
             changeOrigin: true
         })
     )
     app.use(
         '/community',
         createProxyMiddleware({
-            // target: 'http://openjdk4:8080',
-            target: 'http://localhost:8083',
+            target: 'http://openjdk4:8080',
+            // target: 'http://localhost:8083',
             changeOrigin: true
         })
     )
