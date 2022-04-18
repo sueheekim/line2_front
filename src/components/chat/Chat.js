2b2cd1b735e7c20a5af785153b5773e43b6b0871
jeonchangbae <avantgard7@gmail.com>
2022-04-18 14:33:05 +0900

    chat test

--- src/components/chat/Chat.js
+++ src/components/chat/Chat.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import { ChatEngine } from 'react-chat-engine';
+import { useSelector } from 'react-redux';
+import { selectUser } from '../../_reducers';
+// import ChatFeed from './ChatFeed';
 
 export function Chat() {
+    const user = useSelector(selectUser);
+    console.log(user)
 	return (
 		<ChatEngine
 			height='100vh'
-			userName='annsim'
-			userSecret='annsim'
+			userName={user.loginName}
+			userSecret={user.password}
 			projectID='213c154d-5012-4074-8e41-70624aa20dc7'
+            // renderChatFeed ={(chatAppProps) => <ChatFeed {...chatAppProps}/>}
 		/>
 	);
 }
---
+++ src/components/chat/MessageForm.js
@@ -0,0 +1,58 @@
+import React,{useState} from 'react'
+import {sendMessage, isTyping} from 'react-chat-engine';
+import AddPhotoAlternateIcon from '@mui/icons-material/AddPhotoAlternate';
+import UploadIcon from '@mui/icons-material/Upload';
+
+function MessageForm(props) {
+
+    const [value, setValue] =useState('');
+    const {chatId, creds} = props;
+
+    const handleSubmit = (event) =>{
+        event.preventDefault();
+
+        const text = value.trim();
+
+        if(text.length > 0) sendMessage(creds, chatId, {text});
+
+        setValue('');
+    }
+
+    const handleChange = (event) =>{
+        setValue(event.target.value);
+
+        isTyping(props, chatId);
+    }
+
+    const handleUpload =(event) =>{
+        sendMessage(creds, chatId, { files : event.target.files, text: ''})
+    }
+  return (
+    <form className='message-form' onSubmit={handleSubmit}>
+        <input
+            className='message-input'
+            placeholder='send a message...'
+            value={value}
+            onChange={handleChange}
+            onSubmit={handleSubmit}
+        />
+        <label htmlFor='upload-button'>
+            <span className='image-button'>
+                <AddPhotoAlternateIcon className='picture-icon'/>
+            </span>
+            <input 
+                type='file'
+                multiple={false}
+                id='upload-button'
+                style={{display:'none'}}
+                onChange={handleUpload}
+                />
+                <button type='submit' className='send-button'>
+                    <UploadIcon className='send-icon' />
+                </button>
+        </label>
+    </form>
+  )
+}
+
+export default MessageForm
