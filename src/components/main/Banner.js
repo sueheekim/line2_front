9e8a7955fe3ccefbf5c57d14d7448378b16115bb
HD <gudehdu10@gmail.com>
2022-04-20 06:02:55 +0000

    test

--- src/components/main/Banner.js
+++ src/components/main/Banner.js
@@ -5,12 +5,6 @@ function Banner() {
     return (
         <div>
             <div className="banner_img">
-                {/* <img src='http://links.papareact.com/0fm' alt='banner'/> */}
-                <img src={bgImg} alt="banner" />
-                {/* <div className='banner_text'>
-            <p>Not sure where to go? Ansim.</p>
-            <button>Come here</button>
-            </div> */}
             </div>
         </div>
     );
--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -5,11 +5,9 @@ import LargeCard from './LargeCard';
 import Search from './Search';
 import axios from 'axios';
 
-
 function Main({ exploreData, cardsData }) {
     const smallcardUrl = '/home/v1/home/list';
     const [smallCard, setSmallCard] = useState([]);
-
 
     useEffect(() => {
         axios.get(smallcardUrl).then(res => {
@@ -22,24 +20,45 @@ function Main({ exploreData, cardsData }) {
         <div>
             <Search />
             <Banner />
-            <div className="small_card_text">
-                <h2>숙소 바로 예약</h2>
+            <div className="center">
+                <div className="main_title">숙소 바로 예약</div>
             </div>
-            <div className="small_card">
-                {smallCard && smallCard
-                    .map(item => (
-                        <SmallCard
-                            key={item.homeId}
-                            id={item.homeId}
-                            image={item.image}
-                            homeName={item.homeName}
-                            shelter_location={item.shelter_location}
-                        />
-                    ))
-                    .slice(0, 4)}
-            </div>
-            <div className="large_card">
-                <LargeCard img="https://newsimg.hankookilbo.com/cms/articlerelease/2020/07/20/2bc15fde-0663-41a1-a44b-7b1337e1e243.jpg" />
+            <div className="container">
+                <div className="small_card">
+                    {smallCard &&
+                        smallCard
+                            .map(item => (
+                                <SmallCard
+                                    key={item.homeId}
+                                    id={item.homeId}
+                                    image={item.image}
+                                    homeName={item.homeName}
+                                    shelter_location={item.shelter_location}
+                                />
+                            ))
+                            .slice(0, 4)}
+                </div>
+                <div className="large_card">
+                    <div className="idea_img">
+                        <div className="image_wapper">
+                            <div className="idea_text_box">
+                                <div className="ieda_text">안심 서비스의 이념</div>
+                                <div className="ieda_text">안심은 안전한 쉼터를 말합니다.</div>
+                                <div className="ieda_text">
+                                    프로젝트의 초기 단계에서 우리는 가능한 많은 가정 밖 청소년들을 돕고 싶었습니다.
+                                </div>
+                                <div className="ieda_text">
+                                    가정 밖 청소년을 돕는 가장 좋은 방법은 그들을 위해 노력하는 조직과 청소년 간의
+                                    의사소통을 개선하는 것 입니다.
+                                </div>
+                                <div className="ieda_text">
+                                    그래서 안심은 가정 밖 청소년들을 돕는 쉼터와 협력하여 조직의 요구 사항을 반영 하여
+                                    서비스를 제공 합니다.
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     );
