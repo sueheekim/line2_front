0d9822139c51918823083ee4dea5f21a8fb9ad6c
jeonchangbae <avantgard7@gmail.com>
2022-04-20 16:14:22 +0900

    LINE2-21 CheckoutPage modal edit

--- src/components/hostMyPage/HostCheckOut.js
+++ src/components/hostMyPage/HostCheckOut.js
@@ -1,28 +1,92 @@
 import React, { useState, useEffect } from 'react';
 import HostCheckOutCard from './HostCheckOutCard';
 import axios from 'axios';
+import { useSelector } from 'react-redux';
+import { selectUser } from '../../_reducers';
+import { Box, Modal } from '@mui/material';
 
 function HostCheckOut() {
     const checkOutHostUrl = '/book/v1/reservation/home/after_check_out/1';
+    const checkOutUrl = '/book/v1/reservation/accept_check_out';
     const [checkInHost, setCheckInHost] = useState([]);
+    const [modalState, setModalState] = useState(false);
+    const [reservationId, setReservationId] = useState(0);
 
     useEffect(() => {
         axios.get(checkOutHostUrl).then(res => {
             setCheckInHost(res.data);
         });
-    }, [checkInHost]);
- 
+    }, [modalState]); 
+
+
+    const openModal = id => {
+        setModalState(true);
+        setReservationId(id);
+    };
+
+    const closeModal = () => {
+        setModalState(false);
+    };
+
+
+    const handleEdit = () => {
+        axios
+            .put(checkOutUrl, {
+                reservationId: reservationId,
+                message: document.getElementById('host_page_modal_input_text').value,
+            })
+            .then(res => {
+                console.log(res);
+                closeModal();
+            });
+            console.log({
+                reservationId: reservationId,
+                message: document.getElementById('host_page_modal_input_text').value,
+            });
+    };
+
     return (
-        <div>
-            {checkInHost.map(guest => (
-                <HostCheckOutCard
-                    key={guest.id}
-                    guest={guest.user}
-                    room={guest.room}
-                    reservation={guest}
-                />
-            ))}
+        <>
+            <Modal
+                className="reservation_modal_container"
+                open={modalState}
+                onClose={closeModal}
+                aria-labelledby="modal-modal-title"
+                aria-describedby="modal-modal-description"
+            >
+                <Box className={'host_page_modal_box'}>
+                    
+                    <div className="host_page_modal_section">
+                        <div className="host_page_modal_text">퇴소 특이사항 수정</div>
+                        <div className="host_page_modal_input_box">
+                            <textarea
+                                id="host_page_modal_input_text"
+                                className="host_page_modal_input"
+                                placeholder="200자 내외로 간단하게 작성"
+                            />
+                        </div>
+                        <div className="host_page_modal_section center">
+                            <button className="guest_review_reservation_card_button" onClick={handleEdit}>
+                                수정 완료
+                            </button>
+                        </div>
+                    </div>
+                </Box>
+            </Modal>
+            <div className="container">
+                <div className="host_reservation_cards_box">
+                    {checkInHost.map(reservation => (
+                        <HostCheckOutCard
+                            guest={reservation.user}
+                            home={reservation.home}
+                            reservation={reservation}
+                            setReservation={setCheckInHost}
+                            openModal={openModal}
+                        />
+                    ))}
+                </div>
             </div>
+        </>
     );
 }
 
--- src/components/hostMyPage/HostCheckOutCard.js
+++ src/components/hostMyPage/HostCheckOutCard.js
@@ -1,90 +1,56 @@
 import React, { useState } from 'react';
-import {
-    Card,
-    CardContent,
-    CardActionArea,
-    Accordion,
-    AccordionSummary,
-    AccordionDetails,
-    TextField,
-    Button,
-} from '@mui/material';
 import { format } from 'date-fns';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import axios from 'axios';
 
-function HostCheckOutCard({ guest, room, reservation }) {
-    const checkOutUrl = '/book/v1/reservation/accept_check_out';
-    const [editCheckOutMessage, setEditCheckOutMessage] = useState('');
+function HostCheckOutCard({ guest, room, reservation, openModal}) {
 
     const formattedCheckInDate = format(new Date(reservation.checkIn), 'yyyy-MM-dd');
     const formattedCheckOutDate = format(new Date(reservation.checkOut), 'yyyy-MM-dd');
 
-    const handleEdit = () => {
-        axios
-            .put(checkOutUrl, {
-                reservationId: reservation.id,
-                message: editCheckOutMessage,
-            })
-            .then(res => {
-                console.log(res);
-            });
-    };
-
     return (
-        <div className="host_checkinout_card">
-                <Card sx={{ maxWidth: 400 }}>
-                <CardActionArea>
-                    <CardContent className="card_content">
-                    <h4> 퇴소완료일: {formattedCheckOutDate} </h4>
-                    <div className="guest_profile_container">
-                            <div className="guest_profile_img">
-                                <img src={`img/${guest.userImg}`} alt="user.png" />
-                            </div>
-                            <div className="guest_name">
-                                {guest.userName} {guest.userGender}
-                            </div>
-                            <div className="guest_phone">{guest.userPhoneNumber}</div>
-                            <div className="guest_email">{guest.userEmail}</div>
-                        </div>
-                        <div className="host_reservaion_info">
-                        <div className="host_checkout_card_room_name">{room.roomName}</div>
-                        <div className="host_checkout_card_outdate" style={{ fontSize: 'small' }}>
-                        입소완료일 : {formattedCheckInDate}
-                        </div>
-                        </div>
-                    <div className="host_checkin_accordin">
-                    <Accordion>
-                        <AccordionSummary
-                            expandIcon={<ExpandMoreIcon />}
-                            aria-controls="panel1a-content"
-                            id="panel1a-header"
-                        >
-                            <div className="host_checkout_card_outdate">퇴소시 특이사항</div>
-                        </AccordionSummary>
-                        <AccordionDetails>
-                            <TextField
-                                id="standard-textarea"
-                                label={reservation.checkOutMessage}
-                                placeholder="500자 내외로 입력하세요"
-                                multiline
-                                variant="standard"
-                                onChange={({ target: { value } }) => setEditCheckOutMessage(value)}
-                            >
-                                {' '}
-                            </TextField>
-                        </AccordionDetails>
-                        <Button variant="contained" size="small">
-                            입력
-                        </Button>
-                        <Button variant="contained" size="small" color="error" onClick={handleEdit}>
-                            수정
-                        </Button>
-                    </Accordion>
-                    </div>
-                    </CardContent>
-                </CardActionArea>
-                </Card>
+        <div className="host_page_reservation_card">
+            <div
+                className="host_page_reservation_card_img"
+                style={{ backgroundImage: `url("./img/${guest.userImg}")` }}
+            ></div>
+            <div className="host_page_reservation_card_column">
+                <div className="justify-content-space-between">
+                    <p className="host_page_reservation_card_item_title">- 예약 번호:</p>
+                    <p className="host_page_reservation_card_item">{reservation.id}</p>
+                </div>
+                <div className="justify-content-space-between">
+                    <p className="host_page_reservation_card_item_title">* 이름 :</p>
+                    <p className="host_page_reservation_card_item">{guest.userName}</p>
+                </div>
+                <div className="justify-content-space-between">
+                    <p className="host_page_reservation_card_item_title">* 성별 :</p>
+                    <p className="host_page_reservation_card_item">{guest.userGender ? '남자' : '여자'}</p>
+                </div>
+                <div className="justify-content-space-between">
+                    <p className="host_page_reservation_card_item_title">* 연락처 :</p>
+                    <p className="host_page_reservation_card_item">{guest.userPhoneNumber}</p>
+                </div>
+                <div className="justify-content-space-between">
+                    <p className="host_page_reservation_card_item_title">* E-mail :</p>
+                    <p className="host_page_reservation_card_item">{guest.userEmail}</p>
+                </div>
+                <div className="justify-content-space-between">
+                    <p className="host_page_reservation_card_item_title">* 입소 날짜 :</p>
+                    <p className="host_page_reservation_card_item">{formattedCheckInDate}</p>
+                </div>
+                <div className="justify-content-space-between">
+                    <p className="host_page_reservation_card_item_title">* 퇴소 날짜 :</p>
+                    <p className="host_page_reservation_card_item">{formattedCheckOutDate}</p>
+                </div>
+                <div className="justify-content-space-between">
+                    <p className="host_page_reservation_card_item_title">*특이사항:</p>
+                    <p className="host_page_reservation_card_item">{reservation.checkOutMessage}</p>
+                </div>
+                <div className="justify-content-space-evenly">
+                    <button className="host_page_reservation_card_check_in_button" onClick={() => openModal(reservation.id)}>수정</button>
+                </div>
+            </div>
         </div>
     );
 }
